61f90a51cff128d3af23e11061ed69306957a5a9
Fabio Z <zahnerfabio@gmail.com>
2021-01-07 14:50:53 +0100

    Userinput Fix

--- src/Console/Console.java
+++ src/Console/Console.java
@@ -3,10 +3,13 @@ package Console;
 import Invoker.Invoker;
 
 public class Console {
+
+    String userinput;
+
     public Console(){
     }
     public void Start(){
-        String userInput = "readline";
+        String userInput = "readline"; //Last Userinput
         Invoker invoker = new Invoker();
         invoker.executeCommand(userInput);
     }
