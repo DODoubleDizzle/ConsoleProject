83fb3783ad3179618b88b38e445a7f751655f4c5
turzeno <philipp.rueegg@bwz.rappi.ch>
2021-01-28 14:11:09 +0100

    added content to DIRCommand File

--- src/Command/DIRCommand.java
+++ src/Command/DIRCommand.java
@@ -4,20 +4,23 @@ package Command;
 import java.io.File;
 
 public class DIRCommand extends Command {
-    // not working at the moment
-    File dir = new File("C:\\");
-    File[] files = dir.listFiles();
+    @Override
+    public void execute() {
+        // not working at the moment
+        File dir = new File("C:\\");
+        public File[] files = dir.listFiles();
 
         if (files != null && files.length > 0) {
-        for (File file : files) {
-            // Check if the file is a directory
-            if (file.isDirectory()) {
-                // We will not print the directory name, just use it as a new
-                // starting point to list files from
-                listDirectory(file.getAbsolutePath());
-            } else {
-                // We can use .length() to get the file size
-                System.out.println(file.getName() + " (size in bytes: " + file.length()+")");
+            for (File file : files) {
+                // Check if the file is a directory
+                if (file.isDirectory()) {
+                    // We will not print the directory name, just use it as a new
+                    // starting point to list files from
+                    listDirectory(file.getAbsolutePath());
+                } else {
+                    // We can use .length() to get the file size
+                    System.out.println(file.getName() + " (size in bytes: " + file.length() + ")");
+                }
             }
         }
     }
