7be63ebafb3f496bfdff16f2b952bb664ef52b6f
Fabio Z <zahnerfabio@gmail.com>
2021-02-23 11:29:16 +0100

    DELCommandTest part 2

--- src/Command/MOVECommand.java
+++ src/Command/MOVECommand.java
@@ -12,15 +12,7 @@ public class MOVECommand extends Command {
         } else {
             String source = input.split(" ")[0];
             String target = input.split("")[1];
-
-            if (fileSystemItem.getName().equals(input.split(" ")[0])) {
-                if (input.split(" ")[1] != null) {
-                    fileSystemItem.setName(input.split(" ")[1]);
-                    return;
-                } else {
-                    outputWriter.printLine("Directory not found!");
-                }
-            }
+//todo move make v2
         }
     }
 }
