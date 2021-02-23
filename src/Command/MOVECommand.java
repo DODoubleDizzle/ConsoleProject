1d352d617929936b08b493abb165b5084b2f907b
Shahmir <shahmir.saleem@bwz-rappi.ch>
2021-02-23 11:27:51 +0100

    move command

--- src/Command/MOVECommand.java
+++ src/Command/MOVECommand.java
@@ -2,23 +2,27 @@ package Command;
 
 import Console.Console;
 import Console.IOutputWriter;
+import FileSystem.FileSystemItem;
 
 public class MOVECommand extends Command {
 
     @Override
     public void execute(String input, IOutputWriter outputWriter) {
+
         if (input.equals("..")) {
             String newPath = "Console.setCurrentDirectory(Console.getCurrentDirectory().getParentDirectory())";
         } else {
             String source = input.split(" ")[0];
             String target = input.split("")[1];
 
-            if (fileSystemItem.getName().equals(input.split(" ")[0])) {
-                if (input.split(" ")[1] != null) {
-                    fileSystemItem.setName(input.split(" ")[1]);
-                    return;
-                } else {
-                    outputWriter.printLine("Directory not found!");
+            for (FileSystemItem fileSystemItem : Console.getCurrentDirectory().getFileSystemItems()) {
+                if (fileSystemItem.getName().equals(input.split(" ")[0])) {
+                    if (input.split(" ")[1] != null) {
+                        fileSystemItem.setPath(input.split(" ")[1]);
+                        return;
+                    } else {
+                        outputWriter.printLine("Directory not found!");
+                    }
                 }
             }
         }
