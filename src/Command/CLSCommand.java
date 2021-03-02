2a2ba8ae90036f4b34472268a90d55c49e10bc68
DODoubleDizzle <doriano.dipierro@bwz-rappi.ch>
2021-03-02 11:36:09 +0100

    Final commit

--- src/Command/CLSCommand.java
+++ src/Command/CLSCommand.java
@@ -2,13 +2,12 @@ package Command;
 
 import Console.IOutputWriter;
 
-import java.io.IOException;
-
 public class CLSCommand extends Command {
     @Override
     public void execute(String input, IOutputWriter outputWriter) {
         //locked
-        outputWriter.printLine("Coming soon!");
-        System.out.println("Coming soon!");
+        for(int i = 0; i < 20; i++){
+            outputWriter.printLine(" ");
+        }
     }
 }
